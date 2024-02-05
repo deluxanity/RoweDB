24018a64591817a8b1873e54317274c06c2850a6
Lahmacan <158311014+Lahmacan@users.noreply.github.com>
2024-02-05 19:29:41 +0300

    new_game.c

--- src/new_game.c
+++ src/new_game.c
@@ -91,7 +91,7 @@ void CopyTrainerId(u8 *dst, u8 *src)
 
 static void InitPlayerTrainerId(void)
 {
-    u32 trainerId = (Random() << 16) | GetGeneratedTrainerIdLower();
+    u32 trainerId = (00000);
     SetTrainerId(trainerId, gSaveBlock2Ptr->playerTrainerId);
 }
 
